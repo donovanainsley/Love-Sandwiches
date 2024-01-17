94555e1448ca22230d585ac0dbb8e708ca7833b6
donovanainsley <donovan_weise@live.co.uk>
2024-01-17 14:37:55 +0000

    Adjust validation to check that all values entered are intergers

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ def get_sales_data():
     print("Data should be six numbers, seperated by commas")
     print("Example: 10, 20, 30, 40, 50, 60\n")
 
-    data_str = input("Enter your data here:")
+    data_str = input("Enter your data here: ")
     
     # The split() method returns the broke up value as a list.
     sales_data = data_str.split(",")
@@ -34,11 +34,14 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
          print(f"Invalid data: {e}, please try again.\n")
+
+
 
 get_sales_data()
