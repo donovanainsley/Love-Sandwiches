fc3a47766a31ecacbf441f3131a70cf5feae7829
donovanainsley <donovan_weise@live.co.uk>
2024-01-18 20:50:26 +0000

    Added surplus data to surplus worksheet

--- run.py
+++ run.py
@@ -64,7 +64,19 @@ def update_sales_worksheet(data):
     sales_worksheet = SHEET.worksheet('sales')
     sales_worksheet.append_row(data)
 
-    print( "Sales worksheet updated successfully.\n")
+    print("Sales worksheet updated successfully.\n")
+
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet.
+    """
+
+    print("Updating surplus worksheet...\n")
+
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    
+    print("Surplus worksheet updated successfully.\n")
 
 def calculate_surplus_data(sales_row):
     """
@@ -94,7 +106,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
